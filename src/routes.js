95f9b2599078aa9644f2692fae852269eb3c7c48
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-05-22 23:16:21 -0600

    stopped at step 7

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import Home from './components/Home/Home.js';
-import About from './components/About/About.js'
-import {Route, Switch} from 'react-router-dom'
-import ClassList from './components/ClassList/ClassList.js'
+import About from './components/About/About.js';
+import {Route, Switch} from 'react-router-dom';
+import ClassList from './components/ClassList/ClassList.js';
+import Student from './components/Student/Student.js';  
 
 
 export default (
@@ -10,6 +11,7 @@ export default (
         <Route component = {Home} exact path='/'/>
         <Route component ={About} path= '/about'/>
         <Route component = {ClassList} path = '/classlist/:class'/>
+        <Route component = {Student} path = '/student/:id'/>
 
     </Switch>
 
